8a66695e889abea5840da77089d21fe5e4189688
Duynt13 <ngotrieuduy123@gmail.com>
2025-10-24 00:05:53 +0700

    update-api

--- src/main/java/com/mega/warrantymanagementsystem/config/OpenApiConfig.java
+++ src/main/java/com/mega/warrantymanagementsystem/config/OpenApiConfig.java
@@ -1,31 +1,26 @@
-//package com.mega.warrantymanagementsystem.config;
-//
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.info.Info;
-//import io.swagger.v3.oas.models.servers.Server;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-//
-//import java.util.List;
-//
-//@Configuration
-//public class OpenApiConfig {
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        // Lấy base URL tự động từ request hiện tại (http/https + domain + port)
-//        String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
-//
-//        Server dynamicServer = new Server()
-//                .url(baseUrl)
-//                .description("Dynamic server URL (" + baseUrl + ")");
-//
-//        return new OpenAPI()
-//                .info(new Info()
-//                        .title("Warranty Management System API")
-//                        .version("1.0.0")
-//                        .description("API documentation for Warranty Management System"))
-//                .servers(List.of(dynamicServer));
-//    }
-//}
+package com.mega.warrantymanagementsystem.config;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server ngrokServer = new Server()
+                .url("https://balmily-superbenevolent-rubi.ngrok-free.dev")
+                .description("Public via Ngrok");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Warranty Management System API")
+                        .version("1.0.0")
+                        .description("API documentation for Warranty Management System"))
+                .servers(List.of(ngrokServer));
+    }
+}
